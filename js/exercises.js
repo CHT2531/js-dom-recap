d86d409f2766b28eae6cc8e13a3bf424d888b222
mmantle-hud <m.e.mantle@hud.ac.uk>
2023-10-10 08:20:35 +0100

    Update exercises.js

--- js/exercises.js
+++ js/exercises.js
@@ -21,11 +21,11 @@ function doSearch()
 
 //4. Modify the code so that the certificate for each film is displayed next to the film title e.g. Jaws (15)
 
-//5. Modify the code in doSearch() so that it also displays whatever the user has typed into the text box (the user's search term) in the console. 
+//5. Modify the code in doSearch() so that whatever the user has typed into the text box (the user's search term) is displayed in the console. 
 // Again use querySelector to grab hold of the text box before displaying it's value. Put this code at the start of doSearch(). 
 // See https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#working-with-form-controls for an example.
 
-//6. Modify the code in doSearch() so that only films that match the user's search term are displayed i.e. if the user enters "Jaws", only Jaws should be displayed in the console. 
+//6. Modify the code in doSearch() so that only films that match the user's search term are displayed i.e. if the user enters "Jaws", only Jaws should be displayed. 
 // You will need to use a filter e.g. https://github.com/CHT2531/javascript-recap/blob/master/notes.md#filter-example.
 
 // 7. Modify the code so that the user doesn't have to enter the exact search term e.g. if they enter 'ja' they should get Jaws as a result. 
@@ -46,14 +46,14 @@ function doSearch()
 
 //8. A problem with this is that when the user searches again the results get added to the previous results. 
 // Add some code in doSearch() that will remove the previous search results. 
-// See https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#removing-multiple-elements for a similar example.
+// See https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#removing-elements for a similar example.
 
 //9. Modify your application so that the user doesn't have to use the correct case i.e. they can enter 'jaws' and still get the film Jaws as a result. 
 // If you aren't sure how to do this, do some research into the toLowerCase() method of JavaScript strings.
 
 //10. It would be good if the search ran whenever the user types into the search box i.e. they don't have to press the GO button. 
 // Add some code to your app so that the search runs in response to a keyup event.
-// There isn't anyhting on the notes for this, but https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event has an example
+// There isn't anything on the notes for this, but https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event has an example
 
 //11. One problem with the above is that if there isn't a search term we get a full list of films (try typing in a search term and then deleting it). 
 // Modify the code so that it only searches if there is at least one character in the search box. 
